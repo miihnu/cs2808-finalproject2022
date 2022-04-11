8f95af5aecd502ed49b4c5bf4c924a44a02b7fa7
joeldotbot <89935411+joeldotbot@users.noreply.github.com>
2022-04-11 00:07:24 -0400

    added kanyeapi support

---
+++ public/apihandler.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#kanye").addEventListener('click', function() {
+        kanye();
+    })
+})
+
+function kanye() {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", responseReceivedHandler);
+    xhr.responseType = "json";
+    xhr.open("GET", "https://api.kanye.rest/")
+    xhr.send()
+}
+
+function responseReceivedHandler() {
+    console.log(this.response);
+    
+    if (this.status === 200) {
+       // console.log(`Kanye Quote: ${this.response.quote}`);
+
+       let kanyeParagraph = document.getElementById("kanyeQuote");
+       kanyeParagraph.innerHTML = `Kanye Quote: ${this.response.quote}`;
+       
+    } else {
+       console.log("failure");
+       
+    }
+ }
