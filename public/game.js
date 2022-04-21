c0bbc906da84811ca382461fc9548fb98d78ffa0
jane lee <jan33du@gmail.com>
2022-04-21 07:10:03 -0400

    gameee

--- public/game.js
+++ public/game.js
@@ -1,22 +1,48 @@
-//canvas setup 
+
+
 var canvas = document.createElement("canvas");
-canvas.width = 1000;
-canvas.height = 700;
+canvas.width = 900;
+canvas.height = 600;
 canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border:5px solid black";
 document.body.insertBefore(canvas, document.body.firstChild);
 
 ctx = canvas.getContext("2d");
 
-var x = 150,  
-    y = 150,  
+var kanye = new Image();
+kanye.src = "img/ye.PNG";
+
+var coincount = 0;
+
+var bossActive = false;
+var bossEaten = 0;
+
+var coinTemp = new Image();
+coinTemp.src = "img/tempCoin.PNG";
+
+var randomx = Math.floor(Math.random() * (canvas.width - 30));
+var randomy = Math.floor(Math.random() * (canvas.height - 30));
+
+var enemyx = canvas.width / 2;
+var enemyy = canvas.height / 2;
+
+var tx = canvas.width / 2;
+var ty = canvas.height / 2;
+
+
+var x = 150,  //initial x
+    y = 150,  // initial y
     velY = 0,
     velX = 0,
-    speed = 20, 
+    speed = 2.5, // max speed
     friction = 0.98, // friction
     keys = [];
 
-function draw() {
-    requestAnimationFrame(draw);
+function reset() {
+    coincount = 0;
+}
+
+function draw_scene() {
+    requestAnimationFrame(draw_scene);
 
     if (keys[38]) {
         if (velY > -speed) {
@@ -40,35 +66,43 @@ function draw() {
         }
     }
 
-       // apply some friction to y velocity.
-       velY *= friction;
-       y += velY;
-   
-       // apply some friction to x velocity.
-       velX *= friction;
-       x += velX;
-   
-       // bounds checking
-       if (x >= canvas.width) {
-           x = canvas.width;
-       } else if (x <= 5) {
-           x = 5;
-       }
-   
-       if (y > canvas.height) {
-           y = canvas.height;
-       } else if (y <= 5) {
-           y = 5;
-       }
-   
-       // do the drawing
-       ctx.clearRect(0, 0, canvas.width, canvas.height);
-       ctx.beginPath();
-       ctx.arc(x, y, 10, 0, Math.PI * 2);
-       ctx.fill();
+    // apply some friction to y velocity.
+    velY *= friction;
+    y += velY;
+
+    // apply some friction to x velocity.
+    velX *= friction;
+    x += velX;
+
+    // i added 30 to the bounds checking offset because I know my kanye avatar is 30X30 pixels
+    if (x >= canvas.width - kanye.width) {
+        x = canvas.width - kanye.width;
+    } else if (x <= 0) {
+        x = 0;
+    }
+
+    if (y > canvas.height - kanye.height) {
+        y = canvas.height - kanye.height;
+    } else if (y <= 0) {
+        y = 0;
+    }
+
+    upgrade();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.drawImage(kanye, x, y);
+
+    if (coincount == 0 || coincount % 5 != 0) {
+        spawnAlbum();
+    } else {
+        spawnEnemy();
+        spawnHim();
+    }
+
 }
 
-draw();
+draw_scene();
 
 document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
@@ -77,23 +111,139 @@ document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
 
-const gameStatus = {
-	CONTINUE: 1,
-	WIN: 2,
-	LOSE: 3,
-};
+function spawnAlbum() {
+    requestAnimationFrame(spawnAlbum);
+    if (((x < randomx && randomx < x + 30) || (randomx < x && x < randomx + 30) ) && ( (y < randomy && randomy < y + 33) || (randomy < y && y < randomy + 30))) {
+        randomx = Math.floor(Math.random() * (canvas.width - 5));
+        randomy = Math.floor(Math.random() * (canvas.height - 5));
+        coincount++;
+    }
 
-
-function gameStart() {
-
+    //determines if it is drawn
+    if (coincount == 0 || coincount % 5 != 0) {
+        ctx.drawImage(coinTemp, randomx, randomy);
+    }
 }
 
-function makePlayer() {
-
-    this.x = 0;
-    this.y = 0;
-    this.albums = 0;
-    this.status = 0;
-
+function upgrade() {
+    if (coincount <= 5) {
+        kanye.src = "img/ye.PNG";
+    } else if (coincount > 5 && coincount <= 10 && bossEaten == 1) {
+        kanye.src = "img/kanye60.PNG";
+    }
 }
 
+function spawnEnemy() {
+
+    var drake = new Image();
+
+    if (enemyx < x) {
+        if (enemyy < y) {
+            enemyx++;
+            enemyy++;
+        } else if (enemyy > y) {
+            enemyx++;
+            enemyy--;
+        } else {
+            enemyx++;
+        }
+    } else if (enemyx > x) {
+        if (enemyy > y) {
+            enemyx--;
+            enemyy--;
+        } else if (enemyy < y) {
+            enemyx--;
+            enemyy++;
+        } else {
+            enemyx--;
+        }
+    } else {
+        if (enemyy > y) {
+            enemyy--;
+        } else if (enemyy < y) {
+            enemyy++;
+        }
+    }
+
+    if (enemyx >= canvas.width - drake.width) {
+        enemyx = canvas.width - drake.width;
+    } else if (enemyx <= 0) {
+        enemyx = 0;
+    }
+
+    if (enemyy > canvas.height - drake.height) {
+        enemyy = canvas.height - drake.height;
+    } else if (y <= 0) {
+        enemyy = 0;
+    }
+
+    if (((x < enemyx && enemyx < x + drake.width) || (enemyx < x && x < enemyx + drake.width) ) && ( (y < enemyy && enemyy < y + drake.height) || (enemyy < y && y < enemyy + drake.height))) {
+        console.log("game over!!");
+    }
+
+    if (coincount <= 5 && bossEaten == 0) {
+        drake.src = "img/drakebigger.PNG"
+        ctx.drawImage(drake, enemyx, enemyy);
+        
+    } else if (coincount > 5 && coincount <= 10) {
+
+        drake.src = "img/taylor.PNG"
+        ctx.drawImage(drake, enemyx, enemyy)
+    }
+}
+
+function spawnHim() {
+
+    if (tx < x) {
+        if (tx < y) {
+            tx--;
+            ty--;
+        } else if (ty > y) {
+            tx--;
+            ty++;
+        } else {
+            tx--;
+        }
+    } else if (tx > x) {
+        if (ty > y) {
+            tx++;
+            ty++;
+        } else if (ty < y) {
+            tx++;
+            ty--;
+        } else {
+            tx++;
+        }
+    } else {
+        if (ty > y) {
+            ty++;
+        } else if (enemyy < y) {
+            ty--;
+        }
+    }
+
+    if (tx >= canvas.width - kanye.width) {
+        tx = canvas.width - kanye.width;
+    } else if (tx <= 0) {
+        tx = 0;
+    }
+
+    if (ty > canvas.height - kanye.height) {
+        ty = canvas.height - kanye.height;
+    } else if (ty <= 0) {
+        ty = 0;
+    }
+
+    if (bossEaten == 0) {
+        var ill = new Image();
+        ill.src = "img/triangle.PNG";
+        ctx.drawImage(ill, tx, ty);
+    }
+
+    if (((x < tx && tx < x + kanye.width) || (tx < x && x < tx + kanye.width) ) && ( (y < ty && ty < y + kanye.height) || (ty < y && y < ty + kanye.height))) {
+        bossEaten++;
+        coincount++;
+        console.log(coincount);
+    }
+
+}
